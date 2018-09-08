1418f035f732f723792e62c7267df7013d307931
wegamekinglc <scrappedprince.li@gmail.com>
2018-09-08 18:02:19 +0800

    update examples

--- prml/ch1/polynomial_curve_fitting.py
+++ prml/ch1/polynomial_curve_fitting.py
@@ -5,43 +5,9 @@ Created on 2018-9-8
 @author: cheng.li
 """
 
-import math
-import numpy as np
-
-
-def create_curve(n):
-    x = np.linspace(0., 1., num=n)
-    y = np.sin(2. * math.pi * x)
-    return x, y
-    
-
-def add_noise(y, sigma=0.01):
-    y_hat = y + np.random.randn(len(y)) * sigma
-    return y_hat
-
-
-def polynomials(x, order):
-    x_mat = np.zeros((len(x), order + 1))
-    for o in range(order+1):
-        x_mat[:, o] = np.power(x, o)
-    return x_mat
-
-
-def fitting(x, y, order):
-    x_mat = polynomials(x, order)
-    h = np.linalg.inv(x_mat.T @ x_mat) @ x_mat.T
-    beta = h @ y
-    return beta
-
-
-def fitted_curve(x, beta):
-    order = len(beta) - 1
-    x_mat = polynomials(x, order)
-    y_hat = x_mat @ beta
-    return y_hat
-
 
 if __name__ == '__main__':
+    from prml.ch1.utilities import *
     from matplotlib import pyplot as plt
     sigma = 0.2
     orders = [0, 1, 3, 9]
